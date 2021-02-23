cab7660d67ccd449561ea326a54db5dded7e40cc
hariprasad54 <31009432+hariprasad54@users.noreply.github.com>
2021-02-23 14:04:21 +0530

    Day1 Assignment Submitted

---
+++ day1_Assignments/program1.py
@@ -0,0 +1,6 @@
+a = int(input("first num:"))
+b = int(input("second num:"))
+print("addition : ", a+b)
+print("subtraction : ", a-b)
+print("multiplication : ", a*b)
+print("division :", a/b)
---
+++ day1_Assignments/program2.py
@@ -0,0 +1,3 @@
+x = int(input("first num:"))
+y = int(input("second num:"))
+print("x^y is : ", x**y)
